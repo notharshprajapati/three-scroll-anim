78c07f16b772cab090503450c22892e1ecd415bb
notharshprajapati <notharsh0243@gmail.com>
2023-11-08 14:15:57 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./componenets/Experience";
+
+function App() {
+  return (
+    <Canvas camera={{ fov: 64, position: [2.3, 1.5, 2.3] }}>
+      <Experience />
+    </Canvas>
+  );
+}
+
+export default App;
